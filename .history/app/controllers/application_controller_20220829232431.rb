da91ec92d274a654513392c7a6cfae2bbc48e3fa
peterclermy <peterclermy232@gmail.com>
2022-08-29 23:25:56 +0300

    code along

---
+++ .history/app/controllers/application_controller_20220829232431.rb
@@ -0,0 +1,30 @@
+class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  
+
+end
